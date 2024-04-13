304ea697fa5aa20090d66dcae149c2767281e9aa
Kevin Kraaijveld <103686452+kevinkraaijveld@users.noreply.github.com>
2024-04-13 13:12:44 +0200

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
       $date = date("H:i");
       if($date >="06:00" && $date <"12:00"){
         echo '<body style="background-image: url(./backgrounds/morning.png)"';
-        echo '<h1 id="groet">Goede morgen! ^^</h1>';
+        echo '<h1 id="groet">Goeden morgen! ^^</h1>';
         echo '<p id="tijd">Het is nu ' . $date . '</p>';
       }
       else if($date >="12:00" && $date <"18:00"){
@@ -18,19 +18,35 @@
         echo '<h1 id="groet">Goede middag! :)</h1>';
         echo '<p id="tijd">Het is nu ' . $date . '</p>';
       }
-  		else if($date >="18:00" && $date <"00:00"){
-  			echo '<body style="background-image: url(./backgrounds/evening.png)">';
-  			echo '<h1 id="groet">Goede avond!</h1>';
-  			echo '<p id="tijd">Het is nu ' . $date . '</p>';
-  		}
-  		else{
-  			echo '<body style="background-image: url(./backgrounds/evening.png)">';
-  			echo '<h1 id="groet">Goede Nacht!</h1>';
-  			echo '<p id="tijd">Het is nu ' . $date . '</p>';
-  		}
-		?>
+      else if($date >="18:00" && $date <"00:00"){
+  	echo '<body style="background-image: url(./backgrounds/evening.png)">';
+  	echo '<h1 id="groet">Goede avond!</h1>';
+  	echo '<p id="tijd">Het is nu ' . $date . '</p>';
+      }
+      else{
+  	echo '<body style="background-image: url(./backgrounds/evening.png)">';
+  	echo '<h1 id="groet">Goede Nacht!</h1>';
+  	echo '<p id="tijd">Het is nu ' . $date . '</p>';
+      }
+      ?>
+    <p>
+      <?php
+      $time = date("H:i");
+      $currentDST = date("I");
+
+      if ($currentDST) {
+        $wintertime = date("H:i", strtotime($time) - 3600); 
+        echo "Wintertijd: " . $wintertime;
+      } else {
+        $summertime = date("H:i", strtotime($time) + 3600); 
+        echo "Zomertijd: " . $summertime;
+      }
+      ?>
+</p>
+    <div id="backfresh">
+      <a href="https://www.kevii.nl/portfolio">
+      <img id="imgback" alt="Terug naar portfolio" src="../images/Arrows_Back_white.png">
+      <p style="background-color: rgba(255, 255, 0, 0);color: white;" >Terug naar portfolio</p></a>
+    </div>
   </body>
-  <footer>
-        <p>&#169; Made by Kevin Kraaijveld</p>
-    </footer>
 </html>
